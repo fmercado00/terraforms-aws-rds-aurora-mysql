ee066ea95f9b0f60bf7d78100e8b44d6f0dd7dc4
Francisco Mercado <fmercado00@hotmail.com>
2022-10-23 20:55:40 -0500

    refactor: add sns topic to alarms

--- alarms.tf
+++ alarms.tf
@@ -29,11 +29,38 @@ resource "aws_cloudwatch_metric_alarm" "alarm_rds_CPU_writer" {
   statistic           = "Maximum"
   threshold           = var.cw_max_cpu
   alarm_description   = "RDS CPU Alarm for ${aws_rds_cluster.default[0].id} writer"
-  alarm_actions       = [var.cw_sns_topic]
-  ok_actions          = [var.cw_sns_topic]
+  alarm_actions       = [aws_sns_topic.aurora_alarm.arn]
+  ok_actions          = [aws_sns_topic.aurora_alarm.arn]
 
   dimensions = {
     DBClusterIdentifier = aws_rds_cluster.default[0].id
     Role                = "WRITER"
   }
+}
+
+resource "aws_sns_topic" "aurora_alarm" {
+  name = "aurora_alarm"
+  delivery_policy = jsonencode({
+    "http" : {
+      "defaultHealthyRetryPolicy" : {
+        "minDelayTarget" : 20,
+        "maxDelayTarget" : 20,
+        "numRetries" : 3,
+        "numMaxDelayRetries" : 0,
+        "numNoDelayRetries" : 0,
+        "numMinDelayRetries" : 0,
+        "backoffFunction" : "linear"
+      },
+      "disableSubscriptionOverrides" : false,
+      "defaultThrottlePolicy" : {
+        "maxReceivesPerSecond" : 1
+      }
+    }
+  })
+}
+
+resource "aws_sns_topic_subscription" "topic_email_subscription" {
+  topic_arn = aws_sns_topic.aurora_alarm.arn
+  protocol  = "email"
+  endpoint  = "fmercado00@gmail.com"
 }
